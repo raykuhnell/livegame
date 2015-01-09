0501793b92d8e694b765297421f9f8e580746ff1
Ray Kuhnell <raykuhnell@gmail.com>
2015-01-08 23:07:50 -0500

    Initial project commit
    Committed vagrantfile and provisioning script.  Committed express
    generated project.

---
+++ vagrant/provision.sh
@@ -0,0 +1,15 @@
+# Install Git
+apt-get -y install git
+
+# Install MongoDB
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
+apt-get -y update
+apt-get -y install mongodb-org
+
+# Install NodeJS
+apt-get -y install python-software-properties python g++ make
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get -y update
+apt-get -y install nodejs
+npm update -g npm
